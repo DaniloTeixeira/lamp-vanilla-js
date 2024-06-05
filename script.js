57f2cef1cfc376eeb17fefad6bd50cb384050a8a
Danilo Teixeira <danilo.rt@outlook.com>
2024-06-05 14:36:28 -0300

    refactor: fix src path

--- script.js
+++ script.js
@@ -1,80 +1,80 @@
-const lamp = document.querySelector('#lamp');
-const toggleButton = document.querySelector('#toggle-button');
-const restoreLampButton = document.querySelector('#restore-button');
+const lamp = document.querySelector('#lamp')
+const toggleButton = document.querySelector('#toggle-button')
+const restoreLampButton = document.querySelector('#restore-button')
 
-const localhost = 'http://127.0.0.1:5500';
-const onButtonSrc = `${localhost}/lamp/images/on-button.jpg`;
+const localhost = 'http://127.0.0.1:5500'
+const onButtonSrc = `${localhost}/images/on-button.jpg`
 
-let isLampBroken = false;
+let isLampBroken = false
 
-setDisplayNoneOnRestoreButton();
+setDisplayNoneOnRestoreButton()
 
-function setDisplayNoneOnRestoreButton() {
-    restoreLampButton.classList.add('d-none');
+function setDisplayNoneOnRestoreButton () {
+    restoreLampButton.classList.add('d-none')
 }
 
-function removeDisplayNoneOnRestoreButton() {
-    restoreLampButton.classList.remove('d-none');
+function removeDisplayNoneOnRestoreButton () {
+    restoreLampButton.classList.remove('d-none')
 }
 
-function setTurnOnButtonSrc() {
-    toggleButton.src = `${localhost}/lamp/images/on-button.jpg`;
+function setTurnOnButtonSrc () {
+    toggleButton.src = `${localhost}/images/on-button.jpg`
 }
 
-function setTurnOffButtonSrc() {
-    toggleButton.src = `${localhost}/lamp/images/off-button.jpg`;
+function setTurnOffButtonSrc () {
+    toggleButton.src = `${localhost}/images/off-button.jpg`
 }
 
-function setTurnOnLampSrc() {
-    lamp.src = `${localhost}/lamp/images/on-lamp.jpg`;
+function setTurnOnLampSrc () {
+    lamp.src = `${localhost}/images/on-lamp.jpg`
 }
 
-function setTurnOffLampSrc() {
-    lamp.src = `${localhost}/lamp/images/off-lamp.png`;
+function setTurnOffLampSrc () {
+    lamp.src = `${localhost}/images/off-lamp.png`
 }
 
-function setBackgroundBlack() {
-    document.body.style.background = '#000';
+function setBackgroundBlack () {
+    document.body.style.background = '#000'
 }
 
-function turnOnLamp() {
-    const msg = 'A lâmpada está quebrada, faça a troca para poder acendê-la';
+function turnOnLamp () {
+    const msg = 'The lamp is broken, replace it so you can turn it on.'
 
     if (isLampBroken) {
-        alert(msg);
-        return;
+        alert(msg)
+        return
     }
 
-    document.body.style.background = '#fff';
-    setTurnOnLampSrc();
-    setTurnOnButtonSrc();
-    setDisplayNoneOnRestoreButton();
+    document.body.style.background = '#fff'
+    setTurnOnLampSrc()
+    setTurnOnButtonSrc()
+    setDisplayNoneOnRestoreButton()
 }
 
-function turnOffLamp() {
-    setTurnOffLampSrc();
-    setBackgroundBlack();
-    setTurnOffButtonSrc();
+function turnOffLamp () {
+    setTurnOffLampSrc()
+    setBackgroundBlack()
+    setTurnOffButtonSrc()
 }
 
-function toggleButtonSrcImg() {
-    const isTurnOnButton = toggleButton.src === onButtonSrc;
+function toggleButtonSrcImg () {
+    const isTurnOnButton = toggleButton.src === onButtonSrc
 
-    isTurnOnButton ? turnOffLamp() : turnOnLamp();
+    isTurnOnButton ? turnOffLamp() : turnOnLamp()
 }
 
 lamp.addEventListener(('dblclick'), () => {
-    isLampBroken = true;
-    lamp.src = 'images/broken-lamp.png';
+    isLampBroken = true
+    lamp.src = 'images/broken-lamp.png'
 
-    setBackgroundBlack();
-    setTurnOffButtonSrc();
-    removeDisplayNoneOnRestoreButton();
-});
+    setBackgroundBlack()
+    setTurnOffButtonSrc()
+    removeDisplayNoneOnRestoreButton()
+})
 
 restoreLampButton.addEventListener(('click'), () => {
-    isLampBroken = false;
+    isLampBroken = false
 
-    setTurnOffLampSrc();
-    setDisplayNoneOnRestoreButton();
-});
+    setTurnOffLampSrc()
+    setDisplayNoneOnRestoreButton()
+})
